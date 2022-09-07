bd1d4a53ff8a1bf519f09395f9adf2d35ddd56da
庄伟斌 <zhuangweibin@qq.com>
2022-09-07 17:23:06 +0800

    aa

--- my-quartz/src/main/java/com/zhuang/quartz/controller/JobController.java
+++ my-quartz/src/main/java/com/zhuang/quartz/controller/JobController.java
@@ -4,9 +4,7 @@ package com.zhuang.quartz.controller;
 import com.zhuang.quartz.model.ApiResult;
 import com.zhuang.quartz.model.JobVo;
 import org.quartz.*;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("quartz")
@@ -24,7 +21,7 @@ public class JobController {
     private Scheduler scheduler;
 
     @GetMapping("createJob")
-    public ApiResult<String> createJob(@RequestParam(value = "jobGroup", required = false) String jobGroup,
+    public ApiResult<Object> createJob(@RequestParam(value = "jobGroup", required = false) String jobGroup,
                                        @RequestParam("jobName") String jobName,
                                        @RequestParam("jobClass") String jobClass,
                                        @RequestParam(value = "cron", required = false) String cron,
@@ -32,11 +29,11 @@ public class JobController {
     ) throws SchedulerException {
 
         if (cron == null && intervalSeconds == null) {
-            ApiResult.error("参数cron和intervalSeconds不能同时为空！");
+            return ApiResult.error("参数cron和intervalSeconds不能同时为空！");
         }
         JobKey jobKey = new JobKey(jobName, jobGroup);
         if (scheduler.checkExists(jobKey)) {
-            ApiResult.error("job已存在！");
+            return ApiResult.error("job已存在！");
         }
         Class<? extends Job> jobClazz = null;
         try {
@@ -44,10 +41,10 @@ public class JobController {
             if (Job.class.isAssignableFrom(tempJobClazz)) {
                 jobClazz = (Class<? extends Job>) tempJobClazz;
             } else {
-                ApiResult.error("不是job类！");
+                return ApiResult.error("不是job类！");
             }
         } catch (ClassNotFoundException e) {
-            ApiResult.error("类不存在！");
+            return ApiResult.error("类不存在！");
         }
         JobDetail jobDetail = JobBuilder.newJob(jobClazz)
                 .withIdentity(jobKey)
