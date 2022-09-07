5ba37fafba26b3997560210377d71b6a228faa3f
庄伟斌 <zhuangweibin@qq.com>
2022-09-07 17:37:44 +0800

    aa

---
+++ my-quartz/src/main/java/com/zhuang/quartz/job/CleanLogJob.java
@@ -0,0 +1,11 @@
+package com.zhuang.quartz.job;
+
+import org.quartz.JobExecutionContext;
+
+public class CleanLogJob extends BaseJob {
+
+    @Override
+    protected void exec(JobExecutionContext context) {
+
+    }
+}
